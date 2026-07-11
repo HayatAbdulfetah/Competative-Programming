1e5a5617c772b24712d159d6614ede6e640b35f8
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-07-12 00:13:13 +0300

    Create Young Physicist.py

---
+++ Young Physicist.py
@@ -0,0 +1,15 @@
+n = int(input())
+sum_x = sum_y = sum_z = 0
+
+for i in range(n):
+    x, y, z = map(int, input().split())
+    sum_x += x
+    sum_y += y
+    sum_z += z
+
+if sum_x == 0 and sum_y == 0 and sum_z == 0:
+    print("YES")
+else:
+    print("NO")
+
+# problem link --> https://codeforces.com/problemset/problem/69/A
