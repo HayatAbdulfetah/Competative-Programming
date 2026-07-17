c13cc9e1467e12980a83992e9432d376df939d2c
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-07-17 15:11:10 +0300

    Add level completion check for 'I Wanna Be the Guy'
    Implement game completion check based on input levels.

---
+++ I Wanna Be the Guy.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+x = list(map(int, input().split()))
+y = list(map(int, input().split()))
+
+levels = set(x[1:] + y[1:])
+
+if len(levels) == n:
+    print("I become the guy.")
+else:
+    print("Oh, my keyboard!")
+
+# problem link --> https://codeforces.com/problemset/problem/469/A
