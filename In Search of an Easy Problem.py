943c205b5840e7fd5ba2cd9db2439fc07302743c
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-07-20 00:15:21 +0300

    Change problem link in In Search of an Easy Problem.py
    Updated problem link in the script.

---
+++ In Search of an Easy Problem.py
@@ -0,0 +1,9 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+if 1 in a:
+    print("HARD")
+else:
+    print("EASY")
+
+# problem link --> https://codeforces.com/problemset/problem/1030/A
