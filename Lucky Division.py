de3ca5181aebea91de981bfc0cb5d5e212d48818
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-07-24 22:53:29 +0300

    Optimize lucky number check using a list
    Refactor lucky number check to use predefined list.

--- Lucky Division.py
+++ Lucky Division.py
@@ -1,15 +1,9 @@
 n = int(input())
 
-def lucky(x):
-    while x > 0:
-        d = x % 10
-        if d != 4 and d != 7:
-            return False
-        x //= 10
-    return True
+lucky = [4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777]
 
-for i in range(1, n + 1):
-    if lucky(i) and n % i == 0:
+for x in lucky:
+    if n % x == 0:
         print("YES")
         break
 else:
