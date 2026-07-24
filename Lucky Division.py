d4259f6eb899f65a4906d1efe081a4d332b17c57
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-07-24 22:52:56 +0300

    Update Lucky Division.py

--- Lucky Division.py
+++ Lucky Division.py
@@ -1,3 +1,18 @@
+n = int(input())
 
+def lucky(x):
+    while x > 0:
+        d = x % 10
+        if d != 4 and d != 7:
+            return False
+        x //= 10
+    return True
+
+for i in range(1, n + 1):
+    if lucky(i) and n % i == 0:
+        print("YES")
+        break
+else:
+    print("NO")
 
 # problem link --> https://codeforces.com/problemset/problem/122/A
