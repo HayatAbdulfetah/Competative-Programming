438602d98ac27009bff91d6c16876c1865277b31
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-07-28 11:24:31 +0300

    Create YES or YES?.py

---
+++ YES or YES?.py
@@ -0,0 +1,10 @@
+t = int(input())
+for _ in range(t):
+    s = input()
+    
+    if s.upper() == "YES":
+        print("YES")
+    else:
+        print("NO")
+
+# problem link --> https://codeforces.com/problemset/problem/1703/A
