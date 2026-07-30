6c218fcd9eaf4a6d5e14a7fa7052cf8bfc9f5a2d
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-07-31 00:24:49 +0300

    Create Even Odds.py

---
+++ Even Odds.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+
+odd = (n + 1) // 2
+
+if k <= odd:
+    print(2 * k - 1)
+else:
+    print(2 * (k - odd))
+
+# problem link --> https://codeforces.com/problemset/problem/318/A
