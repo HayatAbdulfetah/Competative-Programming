4dd62a5f8a1ce551232c4f002c7079071148672d
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-08-08 11:48:12 +0300

    Implement single number finder using Counter

---
+++ Single Number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums = Counter(nums)
+
+        for num in nums:
+            if nums[num] == 1:
+                return num
