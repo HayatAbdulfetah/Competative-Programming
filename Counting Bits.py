f56cfee8b543758e06918982ef42a8961196dfe7
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-08-15 17:28:49 +0300

    Add alternative loop for counting bits
    Added alternative method to calculate bit counts.

--- Counting Bits.py
+++ Counting Bits.py
@@ -4,4 +4,8 @@ class Solution:
         for i in range(1, n+1):
             ans[i] = ans[i>>1]+(i&1)
             
+        # or you can replace the for loop by the following for loop
+        # for i in range(1, n+1):
+        #     ans[i] = ans[i // 2]+(i % 2)
+            
         return ans
