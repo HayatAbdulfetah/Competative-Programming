11229b2ac6488209b0bc9b0aff656fa741da548b
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-03 23:30:50 +0300

    Add Codeforces problem link as a comment
    Added a comment with the Codeforces problem link.

--- Expression.py
+++ Expression.py
@@ -4,3 +4,4 @@ c = int(input())
 ans = max(a+b*c, a*(b+c), a*b*c, (a+b)*c, a*b+c, a+b+c)
 print(ans)
 
+# codeforces problem link --> https://codeforces.com/problemset/problem/479/A?adcd1e=caf4feocsqvfkr&csrf_token=453fe2b02127694314b90296c3d17cba
