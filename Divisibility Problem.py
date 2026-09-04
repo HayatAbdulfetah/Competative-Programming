d4739858cd958c86ff914b098b66cc27d9d8301d
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-04 14:56:29 +0300

    Update Divisibility Problem.py

--- Divisibility Problem.py
+++ Divisibility Problem.py
@@ -1,1 +1,11 @@
+t = int(input())
+for _ in range(t):
+  a, b = map(int, input().split())
+  rem = a % b
+  if rem == 0:
+    print(0)
+  else:
+    ans = b - rem
+    print(ans)
+
 # codeforces proplem link --> https://codeforces.com/problemset/problem/1328/A
