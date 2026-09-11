0a17f7b1d80a3c7e09100c1064124194fb5d6025
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-11 23:22:43 +0300

    Update Hulk.py

--- Hulk.py
+++ Hulk.py
@@ -1,3 +1,18 @@
+n = int(input())
 
+result = ""
+
+for i in range(n):
+    if i % 2 == 0:
+        result += "I hate"
+    else:
+        result += "I love"
+    if i < n - 1:
+        result += " that "
+
+
+result += " it"
+
+print(result)
 
 # Codeforces problem link --> https://codeforces.com/problemset/problem/705/A
