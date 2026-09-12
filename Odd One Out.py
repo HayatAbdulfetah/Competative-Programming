af1f3e884cac1a63a5994f3617a1707ff5059b8b
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-12 23:22:35 +0300

    Update Odd One Out.py

--- Odd One Out.py
+++ Odd One Out.py
@@ -1,3 +1,14 @@
+t = int(input())
+for i in range(t):
+	a,b,c = map(int,input().split())
+	if a == b and c != a:
+		print(c)
+	elif a == c and b !=a:
+		print(b)
+	elif b == c and a != b:
+		print(a)
+	else:
+		print(a)
 
-
+    
 # Codeforces problem link --> https://codeforces.com/problemset/problem/1915/A
