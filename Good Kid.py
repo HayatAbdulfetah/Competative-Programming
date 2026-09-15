2bc82401d1d96f517b0b8b0ac71b92eeac4a1f21
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-16 00:36:18 +0300

    Implement solution for Codeforces problem 1873/B

--- Good Kid.py
+++ Good Kid.py
@@ -1,1 +1,15 @@
+import math
+
+t = int(input())
+for _ in range(t):
+  n = int(input())
+  a = list(map(int, input().split()))
+
+  x = min(a) + 1
+  a[a.index(min(a))] = x
+
+  ans = math.prod(a)
+
+  print(ans)
+
 # Codeforces problem link --> https://codeforces.com/problemset/problem/1873/B
