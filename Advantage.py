a4c2482d1ad7185aa7495330339bd8441320d9cc
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-16 02:05:58 +0300

    Add functionality to compute differences based on max values
    Implement logic to calculate differences from the two largest numbers in a list.

--- Advantage.py
+++ Advantage.py
@@ -1,3 +1,22 @@
+t = int(input())
 
+for _ in range(t):
+    n = int(input())
+    s = list(map(int, input().split()))
+    
+    ans = []
+    
+    a = sorted(s)
+    
+    max1 = a[-1]
+    max2 = a[-2]
+    
+    for x in s:
+        if x == max1:
+            ans.append(x - max2)
+        else:
+            ans.append(x - max1)
+            
+    print(*ans)
 
 # Codeforces problem link --> https://codeforces.com/problemset/problem/1760/C
