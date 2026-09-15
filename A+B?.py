38efaa386259044a63bd2ed239c7d922fc273bf9
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-16 01:04:20 +0300

    Update A+B?.py

--- A+B?.py
+++ A+B?.py
@@ -1,1 +1,10 @@
+t = int(input())
+for _ in range(t):
+  expression = input()
+  
+  s = expression.split("+")
+  ans = [int(i) for i in s]
+  
+  print(sum(ans))
+
 # Codeforces problem link --> https://codeforces.com/problemset/problem/1772/A
