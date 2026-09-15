374427b816b8a5f8a4e89db1a20b55182e166391
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-15 23:46:51 +0300

    Store maximum value in variable for efficiency
    Optimize calculation of maximum value by storing it in a variable instead of recalculating it in each iteration.

--- Holiday Of Equality.py
+++ Holiday Of Equality.py
@@ -1,10 +1,12 @@
 n = int(input())
 S = 0
 a = [int(i) for i in input().split()]
+x = max(a)
+
 for i in range(n):
-	x = max(a)
 	y = x - a[i]
 	S += y
+	
 print(S)
 
 # Codeforces problem link --> https://codeforces.com/problemset/problem/758/A
