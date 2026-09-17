7921d4ade28818b293bccf3d5b9ee36e58f38241
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-17 22:19:30 +0300

    Create Shoe Shuffling.py

---
+++ Shoe Shuffling.py
@@ -0,0 +1,32 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    s = list(map(int, input().split()))
+
+    p = [0] * n
+    possible = True
+
+    i = 0
+
+    while i < n:
+        j = i
+      
+        while j < n and s[j] == s[i]:
+            j += 1
+
+        if j - i == 1:
+            possible = False
+            break
+
+        for k in range(i, j - 1):
+            p[k] = k + 2
+
+        p[j - 1] = i + 1
+
+        i = j
+
+    if possible:
+        print(*p)
+    else:
+        print(-1)
