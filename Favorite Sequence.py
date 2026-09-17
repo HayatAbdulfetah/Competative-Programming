2905886d11f735f01e93e5a5ebae20568e295936
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-17 22:06:02 +0300

    Update Favorite Sequence.py

--- Favorite Sequence.py
+++ Favorite Sequence.py
@@ -1,1 +1,19 @@
+t = int(input())
+for i in range(t):
+    n = int(input())
+    nums = input().split()
+    left = 0
+    right = len(nums) - 1
+    result = []
+  
+    for j in range(n):
+        if j % 2 == 0:
+        	result.append(nums[left])
+        	left += 1
+        else:
+        	result.append(nums[right])
+        	right -= 1
+          
+    print(" ".join(result))
+
 # codeforces problem link --> https://codeforces.com/problemset/problem/1462/A
