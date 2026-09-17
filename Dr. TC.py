e1645ff149fa6a9b7eac4fe80056b827921d0309
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-17 22:25:02 +0300

    Create Dr. TC.py

---
+++ Dr. TC.py
@@ -0,0 +1,18 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = list(input())
+    count = 0
+  
+    for i in range(n):
+        a = list(s)
+        if a[i] == '0':
+            a[i] = '1'
+        else:
+            a[i] = '0'
+          
+        for char in a:
+            if char == '1':
+                count += 1
+              
+    print(count)
