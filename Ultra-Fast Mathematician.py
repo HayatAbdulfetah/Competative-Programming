5744b5dafab54c088a83eb3a0bc87147d9f0e6f3
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-19 02:03:34 +0300

    Implement string comparison to generate binary result
    Compare two input strings and output a binary result.

--- Ultra-Fast Mathematician.py
+++ Ultra-Fast Mathematician.py
@@ -1,1 +1,16 @@
+l1 = input()
+l2 = input()
+
+result = ""
+a = [i for i in l1]
+b =[i for i in l2]
+
+for i in range(len(a)):
+	if a[i] == b[i]:
+		result += "0"
+	else:
+		result += "1"
+
+print(result)
+
 # codeforces problem link --> https://codeforces.com/problemset/problem/61/A
