7a03d4cd2a9eba4ebef9aa31ae693322fbdc6799
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-21 01:02:10 +0300

    Update Dominant Character.py

--- Dominant Character.py
+++ Dominant Character.py
@@ -1,1 +1,17 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+    
+    if "aa" in s:
+        print(2)
+    elif "aba" in s or "aca" in s:
+        print(3)
+    elif "abca" in s or "acba" in s:
+        print(4)
+    elif "abbacca" in s or "accabba" in s:
+        print(7)
+    else:
+        print(-1)
+
 # Codeforces problem link --> https://codeforces.com/problemset/problem/1605/C
