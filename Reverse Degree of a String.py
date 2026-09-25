aaa69c8f3c9ab82c46b78ed65b8774cc4172040c
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-26 00:52:00 +0300

    Update Reverse Degree of a String.py

--- Reverse Degree of a String.py
+++ Reverse Degree of a String.py
@@ -1,1 +1,8 @@
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        chars = 'zyxwvutsrqponmlkjihgfedcba'
+        ans = 0
+        for i in range(len(s)):
+            ans += (i + 1) * (chars.index(s[i]) + 1)
 
+        return ans
